920a5346c169fa41f34a3e426cff46845bf38101
kathrinmzl <kathrin@muenzel.de>
2025-07-20 14:08:55 +0200

    Add code to calculate two random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,18 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 })
 
+// Create docstring to describe each function. Can be accessed when using the function elsewhere in te code
+/**
+ * The main game loop, called when the script is first loaded
+ * and when the users answer has been processed
+ */
 function runGame() {
+    // Create two random numbers between 1 and 25
+    // Math.floor rounds any floating point number down to a whole number, 
+    // therefore +1 needs to be added to Math.random()*25 to increase the range from 0-24 to 1-25
+    let num1 = Math.floor(Math.random()*25)+1;
+    let num2 = Math.floor(Math.random()*25)+1;
+
 
 }
 
