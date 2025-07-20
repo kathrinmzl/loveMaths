a92dd131f405a342587477f10baf55f52bc68cd1
kathrinmzl <kathrin@muenzel.de>
2025-07-20 13:11:25 +0200

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("Connected!")
+function runGame() {
+
+}
+
+function checkAnswer() {}
+function calculateCorrectAnswer() {}
+function incrementScore(){}
+function incrementWrongAnswer(){}
+function displayAdditionQuestion(){}
+function displaySubtractQuestion(){}
+function displayMultiplyQuestion(){}
+function displayDivisionQuestion(){}
